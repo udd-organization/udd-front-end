17aaf2f30d7f159e6271358fa1fc653e54d7d411
anciii13 <anaa.radovanovic2001@gmail.com>
2025-10-02 23:21:22 +0200

    feat: added navigation buttons for Search Doc and Upload Doc

--- udd-front-end/src/app/navbar/navbar.component.ts
+++ udd-front-end/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { getKeycloak } from '../auth/keycloak.init';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterModule], 
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
